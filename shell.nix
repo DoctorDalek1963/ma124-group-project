3c37bc17e28e4d9f7a1363ef8879dff14b50384f
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-01-22 19:44:44 +0000

    Create Nix shell

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{pkgs ? import <nixpkgs> {}}: let
+  python = pkgs.python3.withPackages (p:
+    with p; [
+      numpy
+      matplotlib
+      pandas
+      scikitlearn
+    ]);
+in
+  pkgs.mkShell {
+    buildInputs = [python];
+  }
